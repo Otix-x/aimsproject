7017a8dbf91a186af1b4d23bd5992d1577a0c2e2
Nguyen Xuan Phuoc <phuoc.nx205232@sis.hust.edu.vn>
2022-12-09 19:42:32 +0700

    Update DVD

--- hust/soict/dsai/aims/media/DigitalVideoDisc.java
+++ hust/soict/dsai/aims/media/DigitalVideoDisc.java
@@ -1,47 +1,43 @@
 package aimsproject.hust.soict.dsai.aims.media;
 
-public class DigitalVideoDisc extends Disc {
-    private int id;
-    private String title;
-    private String category;
+public class DigitalVideoDisc extends Disc implements Playable {
     private String director;
     private int length;
-    private float cost;
     private static int nbDigitalVideoDiscs = 0;
 
     public DigitalVideoDisc(String title) {
         super(title);
-        this.title = title;
         nbDigitalVideoDiscs++;
         this.setId(nbDigitalVideoDiscs);
     }
 
     public DigitalVideoDisc(String title, String category, float cost) {
         super(title, category, cost);
-        this.title = title;
-        this.category = category;
-        this.cost = cost;
         nbDigitalVideoDiscs++;
         this.setId(nbDigitalVideoDiscs);
     }
 
     public DigitalVideoDisc(String title, String category, String director, float cost) {
         super(title, category, cost);
-        this.title = title;
-        this.category = category;
-        this.director = director;
-        this.cost = cost;
         nbDigitalVideoDiscs++;
         this.setId(nbDigitalVideoDiscs);
     }
 
+    public DigitalVideoDisc(int id, String title, String category, String director, float cost) {
+        super(id, title, category, cost);
+        nbDigitalVideoDiscs++;
+        this.setId(nbDigitalVideoDiscs);
+    }
+
+
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
         super(title, category, director, length, cost);
-        this.title = title;
-        this.category = category;
-        this.director = director;
-        this.length = length;
-        this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.setId(nbDigitalVideoDiscs);
+    }
+
+    public DigitalVideoDisc(int id, String title, String category, String director, int length, float cost) {
+        super(id, title, category, director, length, cost);
         nbDigitalVideoDiscs++;
         this.setId(nbDigitalVideoDiscs);
     }
@@ -69,4 +65,14 @@ public class DigitalVideoDisc extends Disc {
         }
         return false;
     }
+
+    public void play(){
+        System.out.println("Playing DVD: " + this.getTitle());
+        System.out.println("DVD length: " + this.getLength());
+    }
+
+    @Override
+	public String toString() {
+		return "DVD - " + this.getId() + " - " + this.getTitle() + " - " + this.getCategory() + " - " + this.getDirector() + " - " + this.getLength() + " - " + this.getCost() + "$" ;
+	}
 }
